2384b9db21b3e214750df7bd37df1debfe4a84a6
MarcRez33 <59151753+MarcRez33@users.noreply.github.com>
2022-07-11 05:17:57 +0200

    fix

--- discourse/ufwDiscourse.sh
+++ discourse/ufwDiscourse.sh
@@ -3,12 +3,12 @@ sudo apt-get update -y
 sudo apt-get install -y ufw
 clear
 sudo ufw reset
-sudo ufw allow to any port 22 proto tcp
+sudo ufw allow ssh
 sudo yes | sudo ufw enable
 sudo wget -O /usr/local/bin/ufw-docker https://github.com/chaifeng/ufw-docker/raw/master/ufw-docker
 sudo chmod +x /usr/local/bin/ufw-docker
 ufw-docker install
 sudo systemctl restart ufw
-ufw-docker allow app 80
-ufw-docker allow app 443
+ufw allow 443
+ufw allow 80
 ufw reload
